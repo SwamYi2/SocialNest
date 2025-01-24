46deab9d967675cac37fcb48380ab894e0e6b2da
SwamYi <swamyi@gmail.com>
2025-01-24 16:01:12 +0630

    init with post microservice

---
+++ social-nest-post/src/main/java/com/swamyi/social_nest_post/SocialNestPostApplication.java
@@ -0,0 +1,13 @@
+package com.swamyi.social_nest_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNestPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNestPostApplication.class, args);
+	}
+
+}
---
+++ social-nest-post/src/test/java/com/swamyi/social_nest_post/SocialNestPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swamyi.social_nest_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNestPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
